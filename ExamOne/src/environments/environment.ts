3c447e6a5b32b200d1f9e3fdbc87c4aca25904be
Collin Moore <collin.moore96@gmail.com>
2017-06-16 23:21:17 -0600

    redid the setup, hopefully we're in business

--- ExamOne/src/environments/environment.ts
+++ ExamOne/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQZOqL4rXIaAqUtUAaQuO7Z5neOBd_TEI",
+    authDomain: "moorect-exam-1.firebaseapp.com",
+    databaseURL: "https://moorect-exam-1.firebaseio.com",
+    projectId: "moorect-exam-1",
+    storageBucket: "moorect-exam-1.appspot.com",
+    messagingSenderId: "857511584872"
+  }
 };
