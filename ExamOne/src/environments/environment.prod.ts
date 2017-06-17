1e462cde705919d779e34c259a510531cc1dcf3d
Collin Moore <collin.moore96@gmail.com>
2017-06-16 22:17:51 -0600

    firebase imports excluding the component specific ones

--- ExamOne/src/environments/environment.prod.ts
+++ ExamOne/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQZOqL4rXIaAqUtUAaQuO7Z5neOBd_TEI",
+    authDomain: "moorect-exam-1.firebaseapp.com",
+    databaseURL: "https://moorect-exam-1.firebaseio.com",
+    projectId: "moorect-exam-1",
+    storageBucket: "moorect-exam-1.appspot.com",
+    messagingSenderId: "857511584872"
+  }
 };
