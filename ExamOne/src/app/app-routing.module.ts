b989a381313edefa3e81930b72a7a08a799c65c9
Collin Moore <collin.moore96@gmail.com>
2017-06-16 23:51:05 -0600

    got the basic structure in, time for sleep

--- ExamOne/src/app/app-routing.module.ts
+++ ExamOne/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from "app/+list/list.component";
+import { SigninComponent } from "app/+signin/signin.component";
+import { RandomComponent } from "app/+random/random.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'list'},
+  { path: 'list', component: ListComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'random/:type', component: RandomComponent },
+  { path: '**', redirectTo: 'list' }
 ];
 
 @NgModule({
