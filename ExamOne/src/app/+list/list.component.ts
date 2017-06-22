3d717fcb7b507e8be8a78be4d4488af93f83936c
Collin Moore <collin.moore96@gmail.com>
2017-06-21 19:53:09 -0600

    kinda working style for list, ergh!

--- ExamOne/src/app/+list/list.component.ts
+++ ExamOne/src/app/+list/list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+interface Abbreviation {
+  abbreviation: string;
+  full: string;
+  $key?: string;
+}
 
 @Component({
   selector: 'app-list',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  readonly abbreviationPath = 'abreviations';
 
-  ngOnInit() {
+  formAbbreviation : Abbreviation = {
+    'abbreviation': '',
+    'full': ''
+  };
+
+  abbreviationStream: FirebaseListObservable<Abbreviation[]>;
+
+  constructor(db: AngularFireDatabase) {
+    this.abbreviationStream = db.list(this.abbreviationPath);
+   }
+
+  ngOnInit(): void{
+  }
+
+  ngOnDestroy(): void{
+
+  }
+
+  onSubmit(): void {
+    try {
+      if (this.formAbbreviation.$key) {
+        this.abbreviationStream.update(this.formAbbreviation.$key, this.formAbbreviation);
+      } else {
+        this.abbreviationStream.push(this.formAbbreviation);
+      }
+    } catch (e) {
+      console.log('Form error:', e);
+    }
+  }
+
+  remove(abbreviationKey: string): void {
+    this.abbreviationStream.remove(abbreviationKey);
   }
 
 }
--- ExamOne/src/app/app.module.ts
+++ ExamOne/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -79,6 +80,7 @@ export const MaterialModules = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
